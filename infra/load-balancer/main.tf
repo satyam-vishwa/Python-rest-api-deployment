3837d9b0779e935d6c97d497cd17a6ae20f2dfba
Satyam Vishwakarma <87614909+satyam-vishwa@users.noreply.github.com>
2024-08-26 13:13:31 +0530

    lb name updated

--- infra/load-balancer/main.tf
+++ infra/load-balancer/main.tf
@@ -9,8 +9,6 @@ variable "ec2_instance_id" {}
 variable "lb_listner_port" {}
 variable "lb_listner_protocol" {}
 variable "lb_listner_default_action" {}
-variable "lb_https_listner_port" {}
-variable "lb_https_listner_protocol" {}
 variable "lb_target_group_attachment_port" {}
 
 output "aws_lb_dns_name" {
@@ -21,13 +19,12 @@ output "aws_lb_zone_id" {
   value = aws_lb.dev_proj_1_lb.zone_id
 }
 
-
-resource "aws_lb" "dev_proj_1_lb" {
+resource "aws_lb" "dev_proj_1_lb0" {
   name               = var.lb_name
   internal           = var.is_external
   load_balancer_type = var.lb_type
   security_groups    = [var.sg_enable_ssh_https]
-  subnets            = var.subnet_ids # Replace with your subnet IDs
+  subnets            = var.subnet_ids
 
   enable_deletion_protection = false
 
@@ -38,27 +35,14 @@ resource "aws_lb" "dev_proj_1_lb" {
 
 resource "aws_lb_target_group_attachment" "dev_proj_1_lb_target_group_attachment" {
   target_group_arn = var.lb_target_group_arn
-  target_id        = var.ec2_instance_id # Replace with your EC2 instance reference
+  target_id        = var.ec2_instance_id
   port             = var.lb_target_group_attachment_port
 }
 
-resource "aws_lb_listener" "dev_proj_1_lb_listner" {
+resource "aws_lb_listener" "dev_proj_1_lb_listner0" {
   load_balancer_arn = aws_lb.dev_proj_1_lb.arn
   port              = var.lb_listner_port
   protocol          = var.lb_listner_protocol
-
-  default_action {
-    type             = var.lb_listner_default_action
-    target_group_arn = var.lb_target_group_arn
-  }
-}
-
-# https listner on port 443
-resource "aws_lb_listener" "dev_proj_1_lb_https_listner" {
-  load_balancer_arn = aws_lb.dev_proj_1_lb.arn
-  port              = var.lb_https_listner_port
-  protocol          = var.lb_https_listner_protocol
-  ssl_policy        = "ELBSecurityPolicy-FS-1-2-Res-2019-08"
 
   default_action {
     type             = var.lb_listner_default_action
